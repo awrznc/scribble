01586bf0a6079c41dab4fbf86519eab781dce10a
awrznc <49797644+awrznc@users.noreply.github.com>
2020-04-09 22:44:17 +0900

    エラトステネスの篩を追加(N=1000000000, time=0m32.681s)

--- c/src/sieve_of_eratosthenes/main.c
+++ c/src/sieve_of_eratosthenes/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <math.h>
-#define N 1000000
+#define N 1000000000
+#define TRUE 1
+#define FALSE 0
 int search_array[N];
 
 // 配列の中身を表示する
 int print_array() {
     for( int i=0; i<N; i++ ) {
-        if(search_array[i] != 0 && search_array[i] != 1){
-            printf("%d\n", search_array[i]);
+        if(search_array[i] == TRUE){
+            printf("%d\n", i);
         }
     }
     return 0;
@@ -17,19 +19,19 @@ int print_array() {
 int main() {
 
     // 配列の初期化
-    for( int i=0; i<N; i++ ) search_array[i] = i;
+    for( int i=0; i<N; i++ ) search_array[i] = TRUE;
+    search_array[0] = FALSE;
+    search_array[1] = FALSE;
 
     // N の平方根まで繰り返し篩をかける
     double square_root = sqrt(N);
     for( int i=2; i<square_root; i++ ) {
-        for( int j=0; j<N; j++ ){
 
-            // 0 の場合はcontinue
-            if( search_array[j] == 0 ) continue;
+        // 0 の場合はcontinue
+        if( search_array[i] == FALSE ) continue;
 
-            if( search_array[j] % i == 0 && search_array[j] != i ) {
-                search_array[j] = 0;
-            }
+        for( int j=i*i; j<N; j+=i ){
+            search_array[j] = FALSE;
         }
     }
 
