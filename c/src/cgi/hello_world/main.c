7736119e9bbf0acba1f92cfef5a1fe21a0f36718
awrznc <49797644+awrznc@users.noreply.github.com>
2020-08-20 16:57:16 +0900

    add cgi sample

---
+++ c/src/cgi/hello_world/main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void main()
+{
+    /* テキスト(HTML)を出力する事を宣言 */
+    printf("Content-type: text/html\n\n");
+
+    /* HTML出力開始 */
+    printf("<HTML><BODY>");
+    printf("こんにちは");
+    printf("</BODY></HTML>");
+    printf("\n");
+    /* HTML出力終了 */
+
+}
