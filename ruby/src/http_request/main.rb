606666436a695096ee1c9bacbfbc1fc13fa321cb
awrznc <49797644+awrznc@users.noreply.github.com>
2020-12-26 18:26:08 +0900

    add open-uri sample

--- ruby/src/http_request/main.rb
+++ ruby/src/http_request/main.rb
@@ -1,2 +1,9 @@
+require 'uri'
+require 'json'
 url = "http://xxx.xxx.xxx.xxx/sample.json"
+
+require 'net/http'
 response = Net::HTTP.get(URI.parse( URI.encode(url) ))
+
+require 'open-uri'
+response = JSON.parse(URI.open(url).read)
