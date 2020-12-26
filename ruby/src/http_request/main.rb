015de354bd3e394198fd9e2993ab22041759c650
awrznc <49797644+awrznc@users.noreply.github.com>
2020-12-26 18:48:50 +0900

    add post sample

--- ruby/src/http_request/main.rb
+++ ruby/src/http_request/main.rb
@@ -1,9 +1,17 @@
 require 'uri'
 require 'json'
-url = "http://xxx.xxx.xxx.xxx/sample.json"
 
-require 'net/http'
-response = Net::HTTP.get(URI.parse( URI.encode(url) ))
+# エンコードが必要な文字がある場合は以下を利用する
+# URI.encode_www_form_component("日本語")
+url = "http://xxx.xxx.xxx.xxx/sample.json"
 
 require 'open-uri'
 response = JSON.parse(URI.open(url).read)
+
+require 'net/http'
+
+# get
+response = Net::HTTP.get(URI.parse( url) )
+
+# post
+response = Net::HTTP.post_form(URI.parse( url ),{'sample' => 'text'})
