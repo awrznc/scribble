e07ea8cab448e446ef04c556aa72c0e57646d9cf
awrznc <49797644+awrznc@users.noreply.github.com>
2020-12-26 18:52:57 +0900

    delete json module

--- ruby/src/http_request/main.rb
+++ ruby/src/http_request/main.rb
@@ -1,17 +1,16 @@
 require 'uri'
-require 'json'
 
 # エンコードが必要な文字がある場合は以下を利用する
 # URI.encode_www_form_component("日本語")
 url = "http://xxx.xxx.xxx.xxx/sample.json"
 
 require 'open-uri'
-response = JSON.parse(URI.open(url).read)
+response = URI.open( url ).read
 
 require 'net/http'
 
 # get
-response = Net::HTTP.get(URI.parse( url) )
+response = Net::HTTP.get(URI.parse( url ) ).body
 
 # post
-response = Net::HTTP.post_form(URI.parse( url ),{'sample' => 'text'})
+response = Net::HTTP.post_form(URI.parse( url ),{'sample' => 'text'}).body
