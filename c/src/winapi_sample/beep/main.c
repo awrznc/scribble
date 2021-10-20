5b58d847df3f528f71d392d6429ac3692ec8065f
awrznc <awrznc@gmail.com>
2021-10-20 19:47:06 +0900

    Add Beep example.

---
+++ c/src/winapi_sample/beep/main.c
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+int main() {
+    Beep( 440, 200 );   // Doe
+    Beep( 494, 200 );   // Ray
+    Beep( 554, 200 );   // Me
+    Beep( 587, 200 );   // Far
+    Beep( 659, 200 );   // Sew
+    Beep( 740, 200 );   // La
+    Beep( 830, 200 );   // Tea
+    Beep( 880, 200 );   // Doe
+
+    return 0;
+}
