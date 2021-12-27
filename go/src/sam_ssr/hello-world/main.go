ad4f3beeba2ee4491a948be9a275caeac3e331cc
awrznc <awrznc@gmail.com>
2021-12-27 18:26:45 +0900

    Added SSR logic.

--- go/src/sam_ssr/hello-world/main.go
+++ go/src/sam_ssr/hello-world/main.go
@@ -1,47 +1,36 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+	"html/template"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
-)
+type Template struct {
+	Title string
+	Body  string
+}
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
+	parsedObject, err := template.ParseFiles("/opt/index.html")
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		panic(err)
 	}
 
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
+	template := Template{"title_string", "body_string"}
+	writer := new(strings.Builder)
+	err = parsedObject.Execute(writer, template)
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
+		panic(err)
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Body: writer.String(),
+		Headers: map[string]string{
+			"Content-Type": "text/html; charset=utf-8",
+		},
 		StatusCode: 200,
 	}, nil
 }
