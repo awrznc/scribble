aa68a059467ea83c36609a36961ad80fff77843f
awrznc <awrznc@gmail.com>
2022-11-27 09:22:13 +0900

    Updated practice of Ruby.

--- ruby/src/practice/main.rb
+++ ruby/src/practice/main.rb
@@ -7,3 +7,7 @@ p %w[hoge fuga piyo]
 # 実行されるファイルが存在するディレクトリの絶対パスを取得
 p File.expand_path(File.dirname(__FILE__))
 # => "/home/scribble/ruby/src/practice"
+
+# 全ての組み合わせを取得(重複なし)
+p ["Hello, ", "Hi, "].product(["World!", "World!", "Ruby!"]).uniq
+# => [["Hello, ", "World!"], ["Hello, ", "Ruby!"], ["Hi, ", "World!"], ["Hi, ", "Ruby!"]]
