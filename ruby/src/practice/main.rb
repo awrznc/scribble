3c126e6b37cae3cd7c8c63a629d7c81572f963e9
awrznc <awrznc@gmail.com>
2022-12-11 09:44:15 +0900

    Added yield example.

--- ruby/src/practice/main.rb
+++ ruby/src/practice/main.rb
@@ -11,3 +11,12 @@ p File.expand_path(File.dirname(__FILE__))
 # 全ての組み合わせを取得(重複なし)
 p ["Hello, ", "Hi, "].product(["World!", "World!", "Ruby!"]).uniq
 # => [["Hello, ", "World!"], ["Hello, ", "Ruby!"], ["Hi, ", "World!"], ["Hi, ", "Ruby!"]]
+
+# 自作ブロック付きメソッド
+def twice(proc)
+  yield proc * 2
+end
+twice(3) do |value|
+  puts value
+  # => 6
+end
