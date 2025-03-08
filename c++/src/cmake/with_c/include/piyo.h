d0f00ec031936bf249fc06f6f659a30dc369a4f5
awrznc@gmail.com <awrznc@gmail.com>
2025-03-08 17:33:54 +0900

    CMakeでビルドした静的ライブラリをCから呼び出す例を追加

---
+++ c++/src/cmake/with_c/include/piyo.h
@@ -0,0 +1,15 @@
+#ifndef PIYO_H
+#define PIYO_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    int return_42();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PIYO_H
---
+++ c++/src/cmake/with_c/test/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "piyo.h"
+
+int main()
+{
+    int result = return_42();
+    printf("%d\n", result);
+    return 0;
+}
